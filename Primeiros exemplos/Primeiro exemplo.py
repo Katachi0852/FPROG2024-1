341236314a8c5c7e1e50cc7a94bc066668831b73
Katachi0852 <162519710+Katachi0852@users.noreply.github.com>
2024-03-06 21:39:38 -0300

    Arquivo primeiro exemplo.py criado

---
+++ Primeiros exemplos/Primeiro exemplo.py
@@ -0,0 +1,23 @@
+# Comando de saída
+print('Hi lorena')
+
+# Criando uma variavel numérica e atribuindo um valor
+num = 10
+
+# Imprimir um valor da varíavel num
+print(num)
+
+# Trocando o valor da varíavel num
+num = -1
+
+print(num)
+
+# Leitura de valor inteiro par a varíavel nome
+nome = input('Digite seu nome e pressione ENTER: ')
+print(nome)
+
+# Leitura de valor inteiro para a varíavel idade
+idade = int(input('Digite sua idade: '))
+
+print('Seu nome é ',nome,' e sua idade é ',idade)
+
