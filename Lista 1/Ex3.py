d430dfad0f910ea2ee56f02b341995ab9ba91df9
Katachi0852 <162519710+Katachi0852@users.noreply.github.com>
2024-03-13 20:47:35 -0300

    Exercicio 3

---
+++ Lista 1/Ex3.py
@@ -0,0 +1,16 @@
+# Exibir a mensagem da questão
+print('Qual é o verdadeiro nome do super-herói Batman?')
+print('a) Bruce Wayne') 
+print('b) Clark Kent')
+print('c) Peter Parker') 
+print('d) Tony Stark') 
+print('e) Steve Rogers')
+
+# Armazenar a resposta correta em uma varriável
+resposta = 'a'
+
+# Perguntar a resposta do usuário e guardar
+respostaUsuário = input('Digite a resposta: ')
+
+# Mensagem informando as respostas
+print('A sua resposta foi ',respostaUsuário,'\nA resposta correta é ',resposta,'.')
---
+++ Lista 1/Ex4.py
@@ -0,0 +1,7 @@
+pergunta = 'Qual é o verdadeiro nome do super-herói Batman?'
+alternativaA = 'Bruce Wayne'
+alternativaB = 'Clark Kent'
+
+print(pergunta)
+print('a) ', alternativaA)
+print('b) ', alternativaB)
