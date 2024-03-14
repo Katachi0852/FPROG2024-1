c7e99b1ebad357208e3d30c2e6407e15267a208c
Katachi0852 <162519710+Katachi0852@users.noreply.github.com>
2024-03-13 22:01:40 -0300

    Adicionado lista 2

---
+++ Lista 2/Ex1.py
@@ -0,0 +1,22 @@
+#Ler a quantidade de minutos para converter
+minutos = float(input('Digite o tempo em minutos: '))
+
+#Transformar em segundos
+segundos = minutos * 60
+
+print(minutos, ' minutos é igual a ',segundos, ' segundos.')
+
+#Conversão de doláres para real
+dolar = float(input('Digite a quantidade de dólares que deseja converter: '))
+
+real = dolar * 4.97
+
+print(dolar, ' doláres equivale a ',real, 'reais.\n')
+
+#
+
+peso = float (input('Digite em gramas o peso do prato: '))
+
+valor = peso / 100 * 40
+
+print('O valor total do prato é igual a R$ ',valor,'.')
